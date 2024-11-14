fb7a445c4dcc9b87ee996d3fcd0f90198641e895
jasoonkp <jasonprakash1214@gmail.com>
2024-11-13 16:30:57 -0800

    Login page works

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,22 +1,51 @@
 # Refer to README under flask-backend directory to setup backend 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from flask_session import Session
+from config import ApplicationConfig
+from flask_bcrypt import Bcrypt
+from models import db, User
+# not sure if i need the above lib
+
 import os
 
 
 load_dotenv()
-
 app = Flask(__name__)
-CORS(app)
+app.config.from_object(ApplicationConfig)
+
+bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+server_session = Session(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 # Configure the SQLAlchemy part of the application
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("PERSONAL_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize SQLAlchemySS
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+
+# Creates personal session token for each user
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    }) 
+
 
 # Define a model for your user table
 class User(db.Model):
@@ -27,46 +56,55 @@ class User(db.Model):
     phone_number = db.Column(db.String(20), nullable=False)
     name = db.Column(db.String(100), nullable=False)
 
-def checkIfEmailExists(email):
-    users = User.query.all()
-    for user in users:
-        if email == user.email:
-            print("email alreay in use")
-            return True
-    print("email not in use")
-    return False
 
+# Registration
+@app.route("/register", methods=["POST"])
+def register_user():
+    email = request.json["email"]
+    password = request.json["password"]
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-    print("Recieved email: ", email)    #debugging
-    print("Recieved password: ", password)  #debugging
+    user_exists = User.query.filter_by(email=email).first() is not None
 
-    if not (checkIfEmailExists(email)):
-        try:
-            # Add user to database
-            new_user = User(email=email, password_hash=password, phone_number='N/A', name='none')
-            db.session.add(new_user)
-            db.session.commit()
-            print("User successfully registered.")  #debugging
-            return jsonify({
-                "message": "User registered successfully", 
-                "code": 0
-                }), 200
-        except Exception as e:
-            db.session.rollback()
-            print("Error during registration:", e) #debugging
-            return jsonify({
-                "message": "Registration failed", 
-                "error": str(e)
-                }), 400
+    if user_exists:
+        return jsonify({"error": "User already exists"}), 409
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password_hash=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    session["user_id"] = new_user.user_id
+
     return jsonify({
-        "message": "Email already in use\nEnter a differnt email",
-        "code": 1
-        }), 200
+        "id": new_user.user_id,
+        "email": new_user.email
+    })
+
+# Sever sided session login authentication
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    if not bcrypt.check_password_hash(user.password_hash, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    session["user_id"] = user.user_id
+
+    return jsonify({
+        "id": user.user_id,
+        "email": user.email
+    })
+
+@app.route("/logout", methods=["POST"])
+def logout_user():
+    session.pop("user_id")
+    return "200"
 
 
 if __name__ == '__main__':
--- newApp/app/login/index.tsx
+++ newApp/app/login/index.tsx
@@ -1,114 +1,55 @@
-import { Text, View, Image, TouchableOpacity, StyleSheet, TextInput } from "react-native";
-import { Link } from "expo-router";
-import React from 'react';
+import React, { useState } from "react";
+import httpClient from "../httpClient";
 
-const Login = () => {
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const logInUser = async () => {
+    console.log(email, password);
+
+    try {
+      const resp = await httpClient.post("//localhost:5000/login", {
+        email,
+        password,
+      });
+
+      window.location.href = "../userHomePage";
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        alert("Invalid credentials");
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {/* Logo */}
-      <Image 
-        source={require('../../assets/images/MilkyWayRepairNoCar.png')} 
-        style={styles.logo}
-      />
-
-      {/* Back Arrow */}
-      <TouchableOpacity style={styles.arrowBackContainer}>
-        <Link href="..">
-          <Image 
-            source={require('../../assets/images/arrowBack.png')} 
-            style={styles.arrowBack}
+    <div>
+      <h1>Log Into Your Account</h1>
+      <form>
+        <div>
+          <label>Email: </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id=""
           />
-        </Link>
-      </TouchableOpacity>
-
-      {/* Input Fields */}
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-
-      {/* Login Button */}
-      <Link href="/userHomePage" asChild>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </Link>
-
-      {/* Don't Have an Account Link */}
-      <TouchableOpacity>
-        <Link href="/register">
-          <Text style={styles.linkText}>Don't have an account?</Text>
-        </Link>
-      </TouchableOpacity>
-    </View>
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id=""
+          />
+        </div>
+        <button type="button" onClick={() => logInUser()}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
-// Styling
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  logo: {
-    width: 200,
-    height: 150,
-    marginTop: 0,
-    resizeMode: 'contain',
-  },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginVertical: 20,
-  },
-  button: {
-    width: 150,
-    padding: 10,
-    marginTop: 20,
-    marginBottom: 20,
-    backgroundColor: '#6B4F9B',
-    borderColor: '#333',
-    borderWidth: 1,
-    borderRadius: 20,
-    alignItems: 'center',      // Center content horizontally
-    justifyContent: 'center',  // Center content vertically
-  },
-  buttonText: {
-    color: '#fff',
-    fontFamily: 'Calibri',
-    fontSize: 23,
-    textAlign: 'center',
-  },
-  linkText: {
-    color: '#6B4F9B',
-    fontSize: 18,
-    textDecorationLine: 'underline',
-  },
-  arrowBackContainer: {
-    position: 'absolute',
-    top: 50,
-    left: 21,
-    width: 53,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  arrowBack: {
-    height: 50,
-    width: 53, 
-  }
-});
-
-export default Login;
+export default LoginPage;
