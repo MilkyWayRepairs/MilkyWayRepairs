0365e7afc4542d6d27e931cd31d712d4a010e03b
Argiovannini <109488500+ARGiovannini@users.noreply.github.com>
2024-11-16 15:19:38 -0800

    fixed the users table 'role'

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -10,10 +10,10 @@ def get_uuid():
 
 # Define a model for your user table
 class User(db.Model):
-    __tablename__ = 'user'
-    user_id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.Enum('user', 'employee', 'admin'), nullable=False)
+    phone_number = db.Column(db.String(20))
+    name = db.Column(db.String(100))
+    role = db.Column(db.String(20))
