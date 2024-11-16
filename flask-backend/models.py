23a311ff58875971d702bf10710a10883fea04a9
Jason Prakash <jasonprakash1214@gmail.com>
2024-11-15 17:57:49 -0800

    login backend setup, not fully finished yet

---
+++ flask-backend/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
+
+# Initialize SQLAlchemySS
+db = SQLAlchemy()
+
+def get_uuid():
+    return uuid4().hex
+
+# Define a model for your user table
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
