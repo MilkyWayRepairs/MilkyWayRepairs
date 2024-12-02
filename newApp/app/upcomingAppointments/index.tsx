b55fe91da013bcfe1dc9048603d5e1a462922f1b
Argiovannini <109488500+ARGiovannini@users.noreply.github.com>
2024-12-01 18:18:50 -0800

    changed 4 space indents to 2 space indents

---
+++ newApp/app/upcomingAppointments/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import newPageTemplate from "../newPageTemplate";
+import { View } from "react-native";
+
+const upcomingAppointments = () => {
+    return (
+        <View>
+            {newPageTemplate()}
+        </View>
+    );
+};
+
+export default upcomingAppointments;
