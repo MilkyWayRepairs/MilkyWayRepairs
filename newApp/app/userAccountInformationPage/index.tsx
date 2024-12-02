f2261b10d744e7105680ba3fa9cbae736c4f639b
CAJALEWI <cjlew1234@gmail.com>
2024-12-01 17:55:03 -0800

    Adding a Template and another file that uses the import

---
+++ newApp/app/userAccountInformationPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewPageTemplate from "../newPageTemplate";
+
+const UserAccountInformationPage = () => {
+  return (
+    <NewPageTemplate>
+      {/* Your account information content here */}
+    </NewPageTemplate>
+  );
+};
+
+export default UserAccountInformationPage;
