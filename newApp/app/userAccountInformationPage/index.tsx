9b625279260458ee328f2c58f1cb3244c5914ac8
Moises Robledo <moisesrobledo12@gmail.com>
2024-12-02 20:57:14 -0800

    Working on account info

--- newApp/app/userAccountInformationPage/index.tsx
+++ newApp/app/userAccountInformationPage/index.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SERVER_URL } from "@/config/config";
 import { Link } from 'expo-router';
-import NewPageTemplate from '../newPageTemplate';
-
 
 interface UserInfo {
   name: string;
@@ -90,30 +88,57 @@ const UserAccountInformationPage = () => {
     console.log('Account button pressed');
   };
 
+  const edit = () => {
+    
+  }
+
   return (
-    <NewPageTemplate title = 'User Account Information'>
-      <View style={styles.container}>
-        <Text style={styles.title}>User Account Information</Text>
-        
-        {/* Basic Text Display */}
-        <Text style={styles.text}>Name: {userInfo.name}</Text>
-        <Text style={styles.text}>Email: {userInfo.email}</Text>
-        <Text style={styles.text}>Phone: {userInfo.phone_number}</Text>
-        
-        {/* Notification Preference Picker */}
-        <View style={styles.pickerContainer}>
-          <Text style={styles.text}>Notification Preference:</Text>
-          <Picker
-            selectedValue={userInfo.notification_preference}
-            style={styles.picker}
-            onValueChange={(itemValue) => handleNotificationPreferenceChange(itemValue)}
-          >
-            <Picker.Item label="Email" value="email" />
-            <Picker.Item label="SMS Message" value="sms" />
-          </Picker>
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>User Account Information</Text>
+      
+      {/* Basic Text Display */}
+      <Text style={styles.text}>Name: {userInfo.name}</Text>
+      <Text style={styles.text}>Email: {userInfo.email}</Text>
+      <Text style={styles.text}>Phone: {userInfo.phone_number}</Text>
+      
+      {/* Notification Preference Picker */}
+      <View style={styles.pickerContainer}>
+        <Text style={styles.text}>Notification Preference:</Text>
+        <Picker
+          selectedValue={userInfo.notification_preference}
+          style={styles.picker}
+          onValueChange={(itemValue) => handleNotificationPreferenceChange(itemValue)}
+        >
+          <Picker.Item label="Email" value="email" />
+          <Picker.Item label="SMS Message" value="sms" />
+        </Picker>
       </View>
-    </NewPageTemplate>
+
+      <View>
+        <TouchableOpacity style={styles.button} onPress={edit}>
+          <Text style={styles.buttonText}>Edit Info</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Bottom Navigation */}
+      <View style={styles.bottomNav}>
+        <TouchableOpacity style={styles.homeButtonContainer}>
+          <Link href="/userHomePage">
+            {"  "}
+            <Image
+              source={require('../../assets/images/homeLogo.png')}
+              style={styles.navIcon}/>
+            {"\n"}
+            <Text style={styles.navText}>Home</Text>
+          </Link>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.accountButtonContainer} onPress={handleAccountPress}>
+          <Image source={require('../../assets/images/accountLogo.png')} style={styles.navIcon} />
+          <Text style={styles.navText}>Account</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
@@ -145,6 +170,47 @@ const styles = StyleSheet.create({
     height: 50,
     width: '100%',
   },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+  },
+  homeButtonContainer: {
+    flex: 1,
+    backgroundColor: '#EBE4EC',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopLeftRadius: 50,
+    borderBottomLeftRadius: 50,
+  },
+  accountButtonContainer: {
+    flex: 1,
+    backgroundColor: '#E5ECE4',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+  },
+  navIcon: {
+    width: 30,
+    height: 30,
+  },
+  navText: {
+    color: 'black',
+  },
+  button: {
+    backgroundColor: '#E0BBE4',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },  
 });
 
 export default UserAccountInformationPage;
